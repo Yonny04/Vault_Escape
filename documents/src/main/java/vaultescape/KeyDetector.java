536cf1e78701aa494f16a033a5e86660e5e55eae
Yoon You <yoon_you@sfu.ca>
2024-10-24 17:56:30 -0700

    Add files via upload

---
+++ documents/src/main/java/vaultescape/KeyDetector.java
@@ -0,0 +1,51 @@
+package vaultescape;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyDetector implements KeyListener{
+
+    public boolean w,a,s,d;
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    //Key pressed
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if(key == KeyEvent.VK_W){
+            w = true;
+        }
+        if(key == KeyEvent.VK_A){
+            a = true;
+        }
+        if(key == KeyEvent.VK_S){
+            s = true;
+        }
+        if(key == KeyEvent.VK_D){
+            d = true;
+        }
+        
+    }
+
+    //KeyReleased
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        if(key == KeyEvent.VK_W){
+            w = false;
+        }
+        if(key == KeyEvent.VK_A){
+            a = false;
+        }
+        if(key == KeyEvent.VK_S){
+            s = false;
+        }
+        if(key == KeyEvent.VK_D){
+            d = false;
+        }
+    }
+    
+}
