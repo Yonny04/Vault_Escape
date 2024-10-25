7a2b372012171044ec4bd8d359263025105276a5
Yoon You <yoon_you@sfu.ca>
2024-10-24 17:56:30 -0700

    Add files via upload

--- documents/src/main/java/vaultescape/App.java
+++ documents/src/main/java/vaultescape/App.java
@@ -1,22 +1,22 @@
 package vaultescape;
-import java.awt.*;
-import java.awt.event.*;
+// import java.awt.*;
+// import java.awt.event.*;
 import javax.swing.JFrame;
 
 public class App extends JFrame{
     public static void main(String[] args) {
         App window = new App();
-
-        window.setSize(500,500);
-        window.setTitle("VaultEscape");
-        
-        
-        window.setVisible(true);
-
-        //add swing objects
+        window.setVisible(true); //visibility of window
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        window.setSize(1980, 1080); //setting the height and width
+        window.setResizable(false); //user cannot resize
+        window.setTitle("VaultEscape"); //title of the window
 
-        
+        GamePanel gp = new GamePanel(); // object for game panel
+
+        window.add(gp); // Adding the board
+        gp.startGameThread(); //start the tic
+
     }
 }
