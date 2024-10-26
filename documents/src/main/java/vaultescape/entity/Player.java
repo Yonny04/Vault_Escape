2c6e964d39383e7a7381f60698e25e117d4dfe94
Yoon You <yoon_you@sfu.ca>
2024-10-26 16:02:28 -0700

    Add files via upload

---
+++ documents/src/main/java/vaultescape/entity/Player.java
@@ -0,0 +1,55 @@
+package vaultescape.entity;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import vaultescape.map.GamePanel;
+import vaultescape.map.KeyDetector;
+
+public class Player extends Entity{
+    GamePanel gp;
+    KeyDetector keyh;
+    public boolean alive = true;
+
+    public Player(GamePanel gp, KeyDetector keyh){
+        this.gp = gp;
+        this.keyh = keyh;
+        setDefault();
+    }
+
+    //Sets default values
+    public void setDefault(){
+        x = 100; 
+        y = 100;
+        speed = 5;
+    }
+
+    // Update method for player entity
+    public void update(){
+        if(keyh.w == true){
+            y -= speed; 
+        }
+        if(keyh.a == true){
+            x -= speed; 
+        }
+        if(keyh.s == true){
+            y += speed; 
+        }
+        if(keyh.d == true){
+            x += speed; 
+        }
+        
+        if(alive == false){
+            //shows menu for when the player dies
+
+            System.out.println("Player Died");
+
+        }
+    }
+
+    //draw method for player entity
+    public void draw(Graphics2D g2){
+        g2.setColor(Color.white); //color of the character
+        g2.fillRect(x, y, gp.tilesize, gp.tilesize); //making the "character"
+    }
+}
