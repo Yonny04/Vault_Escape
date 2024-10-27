3a7c224eb84c5bed2efdc476bc5b90b8ab51331f
VivanPrasad <mvp6@sfu.ca>
2024-10-27 15:25:51 -0700

    removed target, added WIP player animations and spritesheet

--- documents/src/main/java/vaultescape/entity/Player.java
+++ documents/src/main/java/vaultescape/entity/Player.java
@@ -1,7 +1,9 @@
 package vaultescape.entity;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 import vaultescape.map.GamePanel;
 import vaultescape.map.KeyDetector;
@@ -13,10 +15,15 @@ public class Player extends Entity{
     public boolean alive = true;
     Wall wall;
 
+    int direction = 1;
+    double spriteCounter;
+    BufferedImage[][] spritesheet;
+
     public Player(GamePanel gp, KeyDetector keyh){
         this.gp = gp;
         this.keyh = keyh;
         setDefault();
+        setPlayerSpritesheet();
     }
 
     //Sets default values
@@ -26,12 +33,44 @@ public class Player extends Entity{
         speed = 5;
     }
 
+    //Sets player spritesheet
+    public void setPlayerSpritesheet(){
+        try {
+            spritesheet = new BufferedImage[4][3];
+                for (int i = 0; i < 3; i++) {
+                    spritesheet[0][i] = ImageIO.read(getClass().getResourceAsStream(String.format("/entity/player/up%d.png",i+1)));
+                    spritesheet[1][i] = ImageIO.read(getClass().getResourceAsStream(String.format("/entity/player/down%d.png",i+1)));
+                    spritesheet[2][i] = ImageIO.read(getClass().getResourceAsStream(String.format("/entity/player/left%d.png",i+1)));
+                    spritesheet[3][i] = ImageIO.read(getClass().getResourceAsStream(String.format("/entity/player/right%d.png",i+1)));
+                }
+        } catch (Exception e) {e.printStackTrace();}
+    }
+
     // Update method for player entity
     public void update(){
-        if(keyh.w == true)  y -= speed; 
-        if(keyh.a == true)  x -= speed;
-        if(keyh.s == true)  y += speed; 
-        if(keyh.d == true)  x += speed; 
+        // Handle player input for movement
+        if (keyh.w || keyh.a || keyh.s || keyh.d) {
+            if (keyh.w == true) { 
+                y -= speed; 
+                direction = 0;
+            }
+            if (keyh.s == true) { 
+                y += speed; 
+                direction = 1;
+            }
+            if (keyh.a == true) { 
+                x -= speed;
+                direction = 2;
+            }
+            if (keyh.d == true) { 
+                x += speed;
+                direction = 3;
+            }
+            spriteCounter++;
+            if (spriteCounter > 29.0) spriteCounter = 0.0;
+        } else {
+            spriteCounter = 10.0;
+        } 
 
         // Borders for the outer line
         if (x < gp.tilesize) x = gp.tilesize;
@@ -45,7 +84,9 @@ public class Player extends Entity{
 
     //draw method for player entity
     public void draw(Graphics2D g2){
-        g2.setColor(Color.white); //color of the character
-        g2.fillRect(x, y, gp.tilesize, gp.tilesize); //making the "character"
+        BufferedImage image = null;
+        int spriteNum = (int)Math.floor(spriteCounter / 10.0);
+        image = spritesheet[direction][spriteNum];
+        g2.drawImage(image, x, y, gp.tilesize, gp.tilesize, null);
     }
 }
