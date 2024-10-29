80570e894b6d411a21df63810bc6804ccb48adb6
Artyom Borodovitsyn <artyom_borodovitsyn_borodovitsyn@sfu.ca>
2024-10-28 17:23:18 -0700

    Added basic menu with start/exit buttons

--- documents/src/main/java/vaultescape/App.java
+++ documents/src/main/java/vaultescape/App.java
@@ -2,11 +2,43 @@ package vaultescape;
 // import java.awt.*;
 // import java.awt.event.*;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 import vaultescape.map.GamePanel;
+import vaultescape.ui.MenuPanel;
 
 public class App extends JFrame{
+
+    private GamePanel gp;
+    private MenuPanel mp;
+    public App() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1280, 768); 
+        setResizable(false);
+        setTitle("VaultEscape");
+        setLocationRelativeTo(null);  
+        gp = new GamePanel();
+        mp = new MenuPanel(
+            e -> startGame(),  
+            e -> System.exit(0)
+        );
+        setContentPane(mp);
+        setVisible(true);
+    }
+    private void startGame(){
+        setContentPane(gp);
+        revalidate();
+        repaint();
+        gp.requestFocus();
+        gp.startGameThread();
+    }
     public static void main(String[] args) {
+        new App();
+    }
+
+
+
+   /*  public static void main(String[] args) {
         App window = new App();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
         window.setSize(1280, 768); // 4:3 game resolution
@@ -23,5 +55,5 @@ public class App extends JFrame{
         window.repaint();    // Repaint the window
 
         
-    }
+    }*/
 }
