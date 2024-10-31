6c8b339298614fda7ae09f71355e7baf8b8bdf1a
Artyom Borodovitsyn <artyom_borodovitsyn_borodovitsyn@sfu.ca>
2024-10-31 12:45:13 -0700

    Added cameras (need to be fixed)

--- documents/src/main/java/vaultescape/entity/Camera.java
+++ documents/src/main/java/vaultescape/entity/Camera.java
@@ -7,34 +7,60 @@ import vaultescape.map.GamePanel;
 
 public class Camera extends Enemy {
     private int detectionRange;
+    private boolean isPlayerInRange;
+    private long lastDetectionTime = 0;
+    private static final long SPEEDUP_COOLDOWN = 3000;
+    
 
-    public Camera(GamePanel gp) {
+    public Camera(GamePanel gp, int x, int y, int detectionRange) {
         super(gp);
+        this.x = x;
+        this.y = y;
+        this.width = 16; 
+        this.height = 16; 
         setDefault();
     }
 
     // Sets default values
     @Override
     public void setDefault() {
-        x = 200;
-        y = 200;
-        detectionRange = 50; // Detection range of the camera
+        detectionRange = 100;
+        isPlayerInRange = false;
+        setHitbox(16, 16);
     }
 
     // Update method for camera logic
     @Override
     public void update() {
-        // Static camera, no movement
-        // Implement detection logic here if the player comes within the range
+        // Should the logic be here instead ?
+    }
+
+    public boolean isPlayerInRange() {
+        int playerX = gp.getPlayer().getX();
+        int playerY = gp.getPlayer().getY();
+        return Math.sqrt(Math.pow(playerX - x, 2) + Math.pow(playerY - y, 2)) <= detectionRange;
+    }
+
+    public boolean canDetect() {
+        long currentTime = System.currentTimeMillis();
+        return (currentTime - lastDetectionTime) >= SPEEDUP_COOLDOWN;
+    }
+
+    public void recordDetection(){
+        lastDetectionTime = System.currentTimeMillis();
     }
 
     // Draw method for camera entity
     @Override
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.blue); // Color of the camera
-        g2.fillRect(x, y, gp.tilesize - 3, gp.tilesize - 3); // Drawing the camera
-        // Optionally, draw detection range
+        if (image == null) {
+            g2.setColor(Color.ORANGE); 
+            g2.fillRect(x, y, width, height); 
+        } else {
+            super.draw(g2);
+        }
+
         g2.setColor(Color.red);
-        g2.drawOval(x - detectionRange, y - detectionRange, detectionRange * 2, detectionRange * 2);
+        g2.drawOval(x  + width/2 - detectionRange, y + height/2 - detectionRange, detectionRange * 2, detectionRange * 2);
     }
 }
