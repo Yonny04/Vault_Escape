25d3d92c13202efaee8b6744a163db60dda6a833
VivanPrasad <mvp6@sfu.ca>
2024-11-10 01:03:14 -0800

    added AnimationPlayer javadoc

--- src/main/java/vaultescape/utils/Animation.java
+++ src/main/java/vaultescape/utils/Animation.java
@@ -2,6 +2,9 @@ package vaultescape.utils;
 
 import vaultescape.ui.GamePanel;
 
+/**
+ * Represents an animation in the game, managing the frames and playback of the animation.
+ */
 public class Animation {
     int[] track;
     int frames;
@@ -14,6 +17,16 @@ public class Animation {
 
     boolean isPlaying = false;
 
+    /**
+     * Constructs an Animation with the specified parameters.
+     *
+     * @param gp        The game panel associated with this animation.
+     * @param name      The name of the animation.
+     * @param track     An array representing the frame sequence of the animation.
+     * @param frames    The number of frames in the animation.
+     * @param duration  The duration of the animation in seconds.
+     * @param loop      Whether the animation should loop.
+     */
     public Animation(GamePanel gp, String name, int[] track, int frames, float duration, boolean loop) {
         this.name = name;
         this.track = track;
@@ -23,34 +36,51 @@ public class Animation {
         this.frameInterval = duration / (float)frames / 3.0f;
     }
 
+    /**
+     * Returns the current frame of the animation.
+     *
+     * @return The index of the current frame in the track.
+     */
     public int getFrame() {
         int nextFrame = (int)Math.floor((double)frame);
         return track[nextFrame];
     }
 
+    /**
+     * Starts the animation, resetting the frame counter.
+     */
     public void start() {
         isPlaying = true;
         frame = 0.0f;
     }
-    
+
+    /**
+     * Updates the animation, advancing the frame based on the frame interval.
+     * If the animation is finished and looping is enabled, it resets the frame counter.
+     * If looping is not enabled, it stops the animation.
+     */
     public void update() {
-        if (isPlaying) {
-            frame += frameInterval;
-            if (finished()) { 
-                if (loop) frame = 0.0f;
-                else isPlaying = false;
-            }
-        }
+        if (!isPlaying) return;
+        frame += frameInterval;
+        if (!finished()) return;
+        if (loop) frame = 0.0f;
+        else isPlaying = false;
     }
 
+    /**
+     * Checks if the animation has finished playing all frames.
+     *
+     * @return true if the animation has finished, false otherwise.
+     */
     public boolean finished() {
         return (frame + frameInterval >= (float)frames);
     }
 
+    /**
+     * Stops the animation and resets the frame counter.
+     */
     public void stop() {
         isPlaying = false;
         frame = 0.0f;
     }
-
-    
 }
